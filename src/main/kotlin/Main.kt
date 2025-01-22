5b42403170b777d42a1d0a49a109b524a29bb062
sinful <sirus_2003@mail.ru>
2025-01-22 18:48:52 +0300

    Implement quote parser for Hitchhiker's Guide to the Galaxy

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,5 +1,23 @@
-package org.example
+import org.jsoup.Jsoup
 
 fun main() {
-    println("Hello World!")
+    val url = "https://mybook.ru/author/duglas-adams/avtostopom-po-galaktike-restoran-u-konca-vselennoj/citations/"
+
+    try {
+        val document = Jsoup.connect(url).get()
+
+        val quotes = document.select("div.sc-2aegk7-2.bzpNIu")
+
+        if (quotes.isNotEmpty()) {
+            println("Цитаты из произведения \"Автостопом по галактике\":\n")
+
+            for ((index, quote) in quotes.withIndex()) {
+                println("${index + 1}. ${quote.text()}\n")
+            }
+        } else {
+            println("Цитаты не найдены. Проверьте селектор.")
+        }
+    } catch (e: Exception) {
+        println("Ошибка при подключении к странице: ${e.message}")
+    }
 }
